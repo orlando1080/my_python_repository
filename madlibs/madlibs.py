3e3e54ade92b5d228090b6eafd0092568c72a8fc
Lewis Sewell <lewislsewell@gmail.com>
2022-08-06 16:56:36 +0100

    finished

--- madlibs/madlibs.py
+++ madlibs/madlibs.py
@@ -1,35 +1,49 @@
+#! python 3
+
 import json
 import os
 
 class MadLibs:
+    path = '.\\templates'
     def __init__(self, word_description, template):
         self.template = template
         self.word_description = word_description
+        self.user_input = []
+        self.story = None
 
-def get_words_from_user(word_description):
-    print('Please provide the following words:')
-    words = []
-    for desc in word_description:
-        user_input = input(f'A {desc}: ')
-        words.append(user_input)
-    return words
+    @classmethod
+    def from_json(cls, name, path=None):
+        if not path:
+            path = cls.path
+        fpath = os.path.join(path, name)
+        with open(fpath, "r") as f:
+            data = json.load(f)
+        mad_libs = cls(**data)
+        return mad_libs
 
-def build_story(template, words):
-    story = template.format(*words)
-    return story
+    def get_words_from_user(self):
+        print('Please provide the following words:')
+        for desc in self.word_description:
+            ui = input(f'{desc}: ')
+            self.user_input.append(ui)
+        return self.user_input
 
-def get_template(name, path='.\\templates'):
-    fpath = os.path.join(path, name)
-    with open(fpath, "r") as f:
-        data = json.load(f)
-    mad_libs = MadLibs(**data)
-    print(mad_libs.word_description)
+    def build_story(self):
+        story = self.template.format(*self.user_input)
+        return story
 
-template_name = 'make me a video game.json'
-get_template(template_name)
+    def display_story(self):
+        print(story)
 
-# template = 'Back when {} Walberg, was marky mark, this how we used to get the party {}!'
-# words = get_words_from_user(['noun', 'verb'])
-# story = build_story(template, words)
-#
-# print(story)
+def select_template():
+    print('Select a Mad Lib from the following list: ')
+    templates = os.listdir(MadLibs.path)
+    template = input(f'{templates} ')
+    return template
+
+
+temp_name = select_template()
+mad_lib = MadLibs.from_json(temp_name)
+words = mad_lib.get_words_from_user()
+story = mad_lib.build_story()
+mad_lib.display_story()
