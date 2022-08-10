f797ec6896148c74821fa6b52f621c91ced0870c
Lewis Sewell <lewislsewell@gmail.com>
2022-08-11 00:28:16 +0100

    refracted

--- MiniProjects/Hangman.py
+++ MiniProjects/Hangman.py
@@ -12,26 +12,26 @@ while playAgain == True:
     elif play == 'n':
         exit()
     print(f'Welcome to Hangman!')
-    word = getpass(prompt=f'To play, please enter a word (word will not be displayed):').lower()
-    while not word.isalpha():
-        word = getpass(prompt=f'Only enter letters, please enter a word (word will not be displayed):').lower()
+    word = list(getpass(prompt=f'To play, please enter a word (word will not be displayed):').lower())
+    for l in word:
+        while not l.isalpha():
+            word = list(getpass(prompt=f'Only enter letters, please enter a word (word will not be displayed):').lower())
     hangman = ['_' for letter in word]
     print(hangman)
-    wordCopy = '' + word
+    wordCopy = word.copy()
     guessCounter = 6
     for n in range(10):
         letter = input(f'Please guess a letter:').lower()
         while len(letter) > 1 or not letter.isalpha():
             letter = input(f'Please enter 1 letter:').lower()
-        if letter in word:
-            while letter in word:
-                i = word.index(letter)
-                hangman.pop(i)
-                hangman.insert(i, letter)
-                word = word.replace(letter, ' ', 1)
-            if ''.join(hangman) == wordCopy:
+        if letter in wordCopy:
+            while letter in wordCopy:
+                i = wordCopy.index(letter)
+                hangman[i] = letter
+                wordCopy[i] = ' '
+            if hangman == word:
                 print(hangman)
-                print(f'You win! The word is {wordCopy}')
+                print(f"You win! The word is {''.join(word)}")
                 break
             else:
                 print(hangman)
@@ -41,7 +41,7 @@ while playAgain == True:
         if guessCounter == 1:
             print(f'You have {guessCounter} guess left')
         elif guessCounter == 0:
-            print(f'You lose! The word was {wordCopy}')
+            print(f"You lose! The word was {''.join(word)}")
             break
         else:
             print(f'You have {guessCounter} guesses left')
