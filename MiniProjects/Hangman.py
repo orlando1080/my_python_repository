824df3241ea729dd39783f91945dc08dd26a10d8
Lewis Sewell <lewislsewell@gmail.com>
2022-08-12 00:26:24 +0100

    refracted

--- MiniProjects/Hangman.py
+++ MiniProjects/Hangman.py
@@ -1,14 +1,14 @@
 #! python3
 from getpass import getpass
 
-playAgain = True
+play_again = True
 
-while playAgain == True:
+while play_again == True:
     play = input(f'Do you want to play? (Y/N):').lower()
     while play != 'y' and play != 'n':
         play = input(f'Do you want to play? Only enter (Y/N):').lower()
     if play == 'y':
-        playAgain = True
+        play_again = True
     elif play == 'n':
         exit()
     print(f'Welcome to Hangman!')
@@ -18,17 +18,17 @@ while playAgain == True:
             word = list(getpass(prompt=f'Only enter letters, please enter a word (word will not be displayed):').lower())
     hangman = ['_' for letter in word]
     print(hangman)
-    wordCopy = word.copy()
-    guessCounter = 6
+    word_copy = word[:]
+    guess_counter = 6
     for n in range(10):
         letter = input(f'Please guess a letter:').lower()
         while len(letter) > 1 or not letter.isalpha():
             letter = input(f'Please enter 1 letter:').lower()
-        if letter in wordCopy:
-            while letter in wordCopy:
-                i = wordCopy.index(letter)
+        if letter in word_copy:
+            while letter in word_copy:
+                i = word_copy.index(letter)
                 hangman[i] = letter
-                wordCopy[i] = ' '
+                word_copy[i] = ' '
             if hangman == word:
                 print(hangman)
                 print(f"You win! The word is {''.join(word)}")
@@ -37,14 +37,14 @@ while playAgain == True:
                 print(hangman)
                 continue
         print(f'Incorrect')
-        guessCounter -= 1
-        if guessCounter == 1:
-            print(f'You have {guessCounter} guess left')
-        elif guessCounter == 0:
+        guess_counter -= 1
+        if guess_counter == 1:
+            print(f'You have {guess_counter} guess left')
+        elif guess_counter == 0:
             print(f"You lose! The word was {''.join(word)}")
             break
         else:
-            print(f'You have {guessCounter} guesses left')
+            print(f'You have {guess_counter} guesses left')
 else:
     exit()
 
