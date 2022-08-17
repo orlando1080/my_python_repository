780954961fb9d2c09be5d3979d8dc18d3eb79a7d
Lewis Sewell <lewislsewell@gmail.com>
2022-08-18 00:05:59 +0100

    refactor

--- MiniProjects/Hangman.py
+++ MiniProjects/Hangman.py
@@ -3,27 +3,28 @@ from getpass import getpass
 
 play_again = True
 
-while play_again == True:
-    play = input(f'Do you want to play? (Y/N):').lower()
-    while play != 'y' and play != 'n':
-        play = input(f'Do you want to play? Only enter (Y/N):').lower()
-    if play == 'y':
-        play_again = True
-    elif play == 'n':
-        exit()
+while play_again:
+
     print(f'Welcome to Hangman!')
-    word = list(getpass(prompt=f'To play, please enter a word (word will not be displayed):').lower())
+    word = list(getpass(prompt=f'To play, please enter a word (word will not be displayed): ').lower())
+
     for l in word:
         while not l.isalpha():
-            word = list(getpass(prompt=f'Only enter letters, please enter a word (word will not be displayed):').lower())
+            word = list(
+                getpass(prompt=f'Only enter letters, please enter a word (word will not be displayed): ').lower())
     hangman = ['_' for letter in word]
+
     print(hangman)
+
     word_copy = word[:]
+
     guess_counter = 6
+
     for n in range(10):
-        letter = input(f'Please guess a letter:').lower()
+        letter = input(f'Please guess a letter: ').lower()
         while len(letter) > 1 or not letter.isalpha():
-            letter = input(f'Please enter 1 letter:').lower()
+            letter = input(f'Please enter 1 letter: ').lower()
+
         if letter in word_copy:
             while letter in word_copy:
                 i = word_copy.index(letter)
@@ -38,6 +39,7 @@ while play_again == True:
                 continue
         print(f'Incorrect')
         guess_counter -= 1
+
         if guess_counter == 1:
             print(f'You have {guess_counter} guess left')
         elif guess_counter == 0:
@@ -45,7 +47,13 @@ while play_again == True:
             break
         else:
             print(f'You have {guess_counter} guesses left')
-else:
-    exit()
+
+    play = input(f'Do you want to play again? (Y/N): ').lower()
+    while play != 'y' and play != 'n':
+        play = input(f'Do you want to play? Only enter (Y/N): ').lower()
+    if play == 'y':
+        continue
+    elif play == 'n':
+        play_again = False
 
 
