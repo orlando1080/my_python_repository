b6c216c6f3294d6f0d9ae44d77a87606a43f9bb0
orlando1080 <105518695+orlando1080@users.noreply.github.com>
2022-09-12 22:07:46 +0100

    Delete LearningLog directory

--- LearningLog/LearningLogs/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LearninglogsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'LearningLogs'
--- LearningLog/LearningLogs/check_topic_owner.py
+++
@@ -1,7 +0,0 @@
-from django.http import Http404
-
-
-def check_topic_owner(topic, request):
-    """Checks the owner of the topic and return error 404 if not correct user."""
-    if topic.owner != request.user:
-        raise Http404
