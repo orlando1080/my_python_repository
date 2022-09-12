188cd53858d6bc5db7a04fff05ccab449ce9788d
orlando1080 <105518695+orlando1080@users.noreply.github.com>
2022-09-12 18:36:19 +0100

    Delete blog directory

--- blog/blogs/check_blog_owner.py
+++
@@ -1,7 +0,0 @@
-from django.http import Http404
-
-
-def check_blog_owner(blog, request):
-    """Checks the owner of the topic and return error 404 if not correct user."""
-    if blog.owner != request.user:
-        raise Http404
