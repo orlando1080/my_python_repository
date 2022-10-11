bd3cfa058759dc325e3b4660f59e65836ba9f39c
Lewis Sewell <lewislsewell@gmail.com>
2022-10-11 16:51:04 +0100

    latest update with password reset

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path('reset/', auth_views.PasswordResetView.as_view(
         template_name='password_reset.html',
         email_template_name='password_reset_email.html',
-        subject_template_name='password_reset-subject.txt'),
+        subject_template_name='password_reset_subject.txt'),
          name='password_reset'),
     path('reset/done/',
          auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
